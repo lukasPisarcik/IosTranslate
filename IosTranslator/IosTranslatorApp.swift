cd057a63c0092bbcc54fb985e6232f61775f6acc
Lukáš Pisarčík <luky.pisarcik@gmail.com>
2023-10-03 20:08:50 +0200

    Initial Commit

---
+++ IosTranslator/IosTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosTranslatorApp.swift
+//  IosTranslator
+//
+//  Created by Lukáš Pisarčík on 03/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct IosTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
